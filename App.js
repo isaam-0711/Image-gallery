5dde346eac0b8289ca40d163647019a93391868c
Isaam Akhtar <isaam1702@outlook.com>
2025-12-13 18:46:47 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Gallery from "./components/Gallery";
+function App() {
+  return (
+  <>
+   <div className="head-title">Image-gallery</div>
+    <Gallery />
+   
+  </>
+  );
+}
+
+export default App;
